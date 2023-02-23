b82b0f3e9cbbf407c9a8829f0fa50e1a46ba5f60
kdukoelho <kdukoelho@gmail.com>
2023-02-23 17:16:33 -0300

    changed: Order Class and tests

--- Demeter - Classes Prototype/src/demeter/classes/prototype/Order.java
+++ Demeter - Classes Prototype/src/demeter/classes/prototype/Order.java
@@ -3,51 +3,86 @@
  */
 package demeter.classes.prototype;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class Order extends IdObject{
     private final int idCostumer;
-    private List<Vegetable> vegetableList;    
-    private String[] addres = new String[4];
+    private List<Vegetable> vegetableList = new ArrayList();
+    private Addres addres;
     private float totalValue;
     
-    Order(int lastId, int idCostumer, String[] addres){
+    Order(int idCostumer){
         this.idCostumer = idCostumer;
         this.addres = addres;
-        this.totalValue = calculateTotal();
+    }
+    
+    // Getters.
+    
+    public List<Vegetable> getVegetableList(){
+        return vegetableList;
+    }
+    
+    public Vegetable getVegetable(int vegetableId){
+        int index = 0;
+        Vegetable vegetable = null;
+        for (Vegetable veg : vegetableList){
+            if (veg.getId() == vegetableId){
+                vegetable = vegetableList.get(index);
+                break;
+            }
+            index++;
+        }
+       return vegetable;
+    }
+    
+    public float getTotal(){
+        return totalValue;
     }
     
     // Functions.
     
-    public void clearOrder(){
+    public void clear(){
         vegetableList.clear();
+        this.totalValue = calculateTotal();
     }
     
-    public void addProduct(Vegetable vegetable, float resquetedAmmount){
-        vegetable.setAmmountInKg(resquetedAmmount);
-        vegetableList.add(vegetable);
+    public void addProduct(Vegetable vegetable, float resquestedAmmount){
+        if (vegetable.getAmountInKg() > resquestedAmmount){
+            vegetable.setAmmountInKg(resquestedAmmount);
+            vegetableList.add(vegetable);
+            this.totalValue = calculateTotal();
+        }
+        else {
+            vegetableList.add(vegetable);
+        }
     }
     
-    public void removeProduct(Vegetable vegetable){
-        vegetableList.remove(vegetable.getId());
+    public void removeProduct(int vegetableId, float requestedAmmountToExclude){
+        Vegetable toExcludeVeg = null;
+        float newAmmount = 0;
+        for (Vegetable veg : vegetableList){
+            if (veg.getId() == vegetableId){
+                toExcludeVeg = veg;
+                if (requestedAmmountToExclude > 0){
+                    newAmmount = veg.getAmountInKg() - requestedAmmountToExclude;
+                }
+            }
+        } 
+        
+        vegetableList.remove(toExcludeVeg);
+        if (requestedAmmountToExclude > 0) {
+            toExcludeVeg.setAmmountInKg(newAmmount);
+            vegetableList.add(toExcludeVeg);
+        }
     }
     
-    private float calculateTotal(){
+    public float calculateTotal(){
         float tot = 0;
         for (Vegetable veg : vegetableList){
             tot += veg.getTotalValue();
         }
         return tot;
-    }
-    
-    public List<Vegetable> finalizeOrder(){   
-        return vegetableList;
-    }
-    
-    // Getters.
-    
-    public float getTotal(){
-        return totalValue;
     }
 }
---
+++ Demeter - Classes Prototype/test/demeter/classes/prototype/OrderTest.java
@@ -0,0 +1,88 @@
+package demeter.classes.prototype;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Cadu
+ */
+public class OrderTest {
+    Order order;
+    
+    Vegetable[] vegetableArray = {new Vegetable("Cenoura", "Legume", 5.2f, 4.50f), 
+                               new Vegetable("Alface", "Verdura", 3.5f, 3f),
+                               new Vegetable("Morango", "Fruta", 5f, 22.5f)};
+    
+    Vegetable[] vegetableArrayClone = vegetableArray.clone();
+
+    
+    @Before
+    public void setUp() {
+        System.out.println("setUp");
+        this.order = new Order(0);
+        this.vegetableArray = vegetableArrayClone;
+                               
+        for (Vegetable veg : vegetableArray){
+            order.addProduct(veg, veg.getAmountInKg());
+        }
+    }
+    
+    @Test
+    public void addProduct_Should_add_product_at_order_list(){
+        Vegetable newVegetable = new Vegetable("Kiwi", "Fruta", 3, 15.4f);
+        
+        order.addProduct(newVegetable, 5);
+        
+        Vegetable[] expectedArray = {vegetableArray[0], vegetableArray[1], vegetableArray[2], newVegetable};
+        Vegetable[] actualArray = (Vegetable[]) order.getVegetableList().toArray(expectedArray);
+        
+        assertArrayEquals(expectedArray, actualArray);
+    }
+    
+    @Test
+    public void removeProduct_Should_remove_product_from_order_list(){
+        order.removeProduct(1, 0);
+        
+        Vegetable[] expectedArray = {vegetableArray[0], vegetableArray[2]};
+        Vegetable[] actualArray = (Vegetable[]) order.getVegetableList().toArray(expectedArray);
+        
+        assertArrayEquals(expectedArray, actualArray);
+    }
+   
+    @Test
+    public void removeProduct_Should_remove_an_especific_ammount_of_product_from_order_list(){
+        order.removeProduct(1, 2);
+        
+        vegetableArray[1].setAmmountInKg(1.5f);
+        Vegetable[] expectedArray = {vegetableArray[0], vegetableArray[1], vegetableArray[2]};
+        Vegetable[] actualArray = (Vegetable[]) order.getVegetableList().toArray(expectedArray);
+        
+        assertArrayEquals(expectedArray, actualArray);
+    }
+    
+    @Test
+    public void calculateTotal_Should_return_total_order_value(){
+        float expectedValue = vegetableArray[0].getTotalValue() + vegetableArray[1].getTotalValue() + vegetableArray[2].getTotalValue(); 
+        float actualValue = order.calculateTotal();
+        
+        assertEquals(expectedValue, actualValue, 146.4f);
+    }
+    
+    @Test
+    public void getVegetable_Should_return_the_correct_vegetable(){
+        Vegetable expectedVegetable = vegetableArray[2];
+        Vegetable actualVegetable = order.getVegetable(3);
+        
+        assertEquals(expectedVegetable, actualVegetable);
+    }
+    
+    @Test
+    public void getVegetableList_Should_return_vegetable_list(){
+        Vegetable[] expectedArray = vegetableArray;
+        Vegetable[] actualArray = (Vegetable[]) order.getVegetableList().toArray(expectedArray);
+        
+        assertArrayEquals(expectedArray, actualArray);
+    }
+}
