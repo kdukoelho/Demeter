e4e5864b9a57abd99ffac2a165ddc6bf7a268e2c
kdukoelho <kdukoelho@gmail.com>
2023-02-23 18:26:45 -0300

    added: Addres tests

---
+++ Demeter - Classes Prototype/test/demeter/classes/prototype/CostumerTest.java
@@ -0,0 +1,51 @@
+package demeter.classes.prototype;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author kdukoelho
+ */
+public class CostumerTest {
+    Costumer costumer;
+   
+    @Before
+    public void setUp() {
+        costumer = new Costumer("Cadu", "cadu@gmail.com", "21 981141198", "RJ", "Rio de Janeiro", "Lapa", "Rua dos Gnomos", "420");
+    }
+   
+    @Test
+    public void getName_Should_return_costumer_name() {
+        String expected = "Cadu";
+        String actual = costumer.getName();
+        
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getEmail_Should_return_costumer_email() {
+        String expected = "cadu@gmail.com";
+        String actual = costumer.getEmail();
+        
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getPhoneNumber_Should_return_costumer_phone_number() {
+        String expected = "21 981141198";
+        String actual = costumer.getPhoneNumber();
+        
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getAddres_Should_return_costumer_addres_object() {
+        Object actual = costumer.getAddres();
+        
+            assertTrue(actual instanceof Addres);
+           
+    }
+    
+}
