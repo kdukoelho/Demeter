78fc1ad1d94a06393e2852245c1ce606fdf221f6
kdukoelho <kdukoelho@gmail.com>
2023-02-27 09:30:59 -0300

    modified: DateManipulator tests

--- Demeter - Classes Prototype/test/demeter/classes/prototype/DateManipulatorTest.java
+++ Demeter - Classes Prototype/test/demeter/classes/prototype/DateManipulatorTest.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
- */
 package demeter.classes.prototype;
 
 import java.util.Date;
@@ -12,7 +8,7 @@ import static org.junit.Assert.assertEquals;
 
 /**
  *
- * @author Cadu
+ * @author github.com/kdukoelho
  */
 public class DateManipulatorTest {
     static Date dateTime;
