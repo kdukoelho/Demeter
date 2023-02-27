4a7e43a8bc2df104054667f5a3cc10e32adfd5af
kdukoelho <kdukoelho@gmail.com>
2023-02-27 09:19:00 -0300

    added: DateManipulator Tests

---
+++ Demeter - Classes Prototype/test/demeter/classes/prototype/DateManipulatorTest.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package demeter.classes.prototype;
+
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+/**
+ *
+ * @author Cadu
+ */
+public class DateManipulatorTest {
+    static Date dateTime;
+    
+    @Before
+    public void setup(){
+        dateTime = new Date();
+    }
+    
+    @Test
+    public void dateNow_Should_return_the_actual_date() {
+        String expectedDate = new SimpleDateFormat("yyyy-MM-dd").format(dateTime);
+        String actualDate = DateManipulator.date_Now();
+        
+        assertEquals(expectedDate, actualDate);
+    }
+    
+    @Test
+    public void timeNow_Should_return_the_actual_hour(){
+        String expectedHour = new SimpleDateFormat("HH:mm:ss").format(dateTime);
+        String actualHour = DateManipulator.time_Now();
+        
+        assertEquals(expectedHour, actualHour);
+    }
+    
+}
