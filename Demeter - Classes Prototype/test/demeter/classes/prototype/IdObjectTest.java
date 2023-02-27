212f9693a79ae35eb1f04f83b5f04e8c9c8bf5b4
kdukoelho <kdukoelho@gmail.com>
2023-02-27 09:31:59 -0300

    added: IdObject tests

---
+++ Demeter - Classes Prototype/test/demeter/classes/prototype/IdObjectTest.java
@@ -0,0 +1,32 @@
+package demeter.classes.prototype;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author github.com/kdukoelho
+ */
+public class IdObjectTest {
+    IdObject[] idObjectsArray = new IdObject[5];
+    
+    @Before
+    public void setUp() {
+        for (int i = 0; i < 5; i++){
+            idObjectsArray[i] = new IdObject();
+        }
+    }
+    
+    @Test
+    public void getId_Should_return_the_id_of_actual_object() {
+        int expectedId = 3;
+        int actualId = idObjectsArray[2].getId();
+        
+        assertEquals(expectedId, actualId);
+    }
+    
+}
